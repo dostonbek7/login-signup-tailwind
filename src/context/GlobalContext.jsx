56638777ceff869f5cf9f66025c11ecb53baa3e6
DostonbekMusayev <dostonbekmusayev72@gmail.com>
2023-12-19 10:36:03 +0500

    add context

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useReducer } from "react";
+export const GlobalContext = createContext();
+
+const reducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "LOGIN":
+      return { ...state, user: payload };
+    case "IS_AUTH_CHANGE":
+      return { ...state, isAuthChange: true };
+      default:
+        return state
+  }
+};
+
+export function GlobalContextProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, {
+    user: null,
+    isAuthChange: false,
+  });
+  return <GlobalContext.Provider value={{...state, dispatch}}>
+    {children}
+  </GlobalContext.Provider>
+}
