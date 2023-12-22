d822b9c60e5bc31b11790e7f70b28bcd5221d410
DostonbekMusayev <dostonbekmusayev72@gmail.com>
2023-12-22 09:59:32 +0500

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-neutral-400 py-4 text-center font-medium dark:bg-slate-800 dark:text-slate-200'>
+    <div className=" absolute bottom-0 left-0 right-0 bg-neutral-400 py-4 text-center font-medium dark:bg-slate-800 dark:text-slate-200">
       <p>All right reserved. {new Date().getFullYear()}</p>
     </div>
   )
